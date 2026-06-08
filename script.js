66c49452c2228ba82d39a722a0fb9db65e1bb554
mitchlian <angelbeattm@gmail.com>
2026-06-08 17:14:58 +0800

    Setup qrscanner
    created the frontend that scans a qrcode.
    
    shows an alert on the link. (Does not redirect you)

---
+++ script.js
@@ -0,0 +1,27 @@
+// script.js file
+
+function domReady(fn) {
+    if (
+        document.readyState === "complete" ||
+        document.readyState === "interactive"
+    ) {
+        setTimeout(fn, 1000);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+}
+
+domReady(function () {
+
+    // If found your qr code
+    function onScanSuccess(decodeText, decodeResult) {
+        alert("Your QR is: " + decodeText);
+        console.log('decodeResult:', decodeResult);
+    }
+
+    let htmlscanner = new Html5QrcodeScanner(
+        "my-qr-reader",
+        { fps: 10, qrbox: 250 }
+    );
+    htmlscanner.render(onScanSuccess);
+});
