c9eae56e0db3db4ad28f550769c1910e19a3129f
XK-School <xavierkwek2002@gmail.com>
2026-06-18 21:13:03 +0800

    Fixed issue with camera not working for demo

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  basicSsl(),
+  plugins: [
+    react(),
+    basicSsl(),
+  ],
 })
